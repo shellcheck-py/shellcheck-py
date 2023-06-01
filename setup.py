95655100f4d647793011863f513b417270988d4d
Anthony Sottile <asottile@umich.edu>
2023-06-01 18:37:45 -0400

    use exist_ok for making build directory

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ def extract(url: str, data: bytes) -> bytes:
 def save_executable(data: bytes, base_dir: str):
     exe = 'shellcheck' if sys.platform != 'win32' else 'shellcheck.exe'
     output_path = os.path.join(base_dir, exe)
-    os.makedirs(base_dir)
+    os.makedirs(base_dir, exist_ok=True)
 
     with open(output_path, 'wb') as fp:
         fp.write(data)
